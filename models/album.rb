fe7bfa2e83575b3f586baffa723e1f50839b7b66
David Eastcroft <davideastcroft95@gmail.com>
2020-03-25 16:20:44 +0000

    Adds initialization and save method.

---
+++ models/album.rb
@@ -0,0 +1,31 @@
+require_relative('../db/sql_runner.rb')
+
+class Album
+
+  attr_accessor :title, :genre, :artist_id
+  attr_reader :id
+
+  def initialize(album)
+    @title = album['title']
+    @genre = album['genre']
+    @id = album['id'].to_i if album['id']
+    @artist_id = album['artist_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO albums
+    (
+      artist_id,
+      title,
+      genre
+    )
+    VALUES
+    (
+      $1, $2, $3
+    )
+    RETURNING id"
+    values = [@artist_id, @title, @genre]
+    @id = SQLRunner.run(sql, values)[0]['id'].to_i
+  end
+
+end
