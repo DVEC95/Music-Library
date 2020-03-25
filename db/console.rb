cd1cd8bb481562b88f1be8508aac7a363ea96d54
David Eastcroft <davideastcroft95@gmail.com>
2020-03-25 17:03:39 +0000

    Adds more sample data.

--- db/console.rb
+++ db/console.rb
@@ -2,19 +2,73 @@ require("pry")
 require_relative("../models/artist")
 require_relative("../models/album")
 
+# ARTIST DATA ---------------
 artist1 = Artist.new({
-  'name' => 'Kraftwerk'
+  'name' => 'Radiohead'
   })
 
 artist1.save()
 
-album1 = Album.new({
-  'artist_id' => artist1.id,
-  'title' => 'Computer World',
-  'genre' => 'Electronic',
+artist2 = Artist.new({
+  'name' => 'Daft Punk'
   })
 
+artist2.save()
+
+artist3 = Artist.new({
+  'name' => 'Kraftwerk'
+  })
+
+artist3.save()
+
+
+# ALBUM DATA -----------------
+album1 = Album.new({
+  'artist_id' => artist1.id,
+  'title' => 'OK Computer',
+  'genre' => 'Alternative Rock'
+  })
 album1.save()
+
+album2 = Album.new({
+  'artist_id' => artist1.id,
+  'title' => 'In Rainbows',
+  'genre' => 'Alternative Rock'
+  })
+album2.save()
+
+album3 = Album.new({
+  'artist_id' => artist2.id,
+  'title' => 'Homework',
+  'genre' => 'Electronic'
+  })
+
+album3.save()
+
+album4 = Album.new({
+  'artist_id' => artist2.id,
+  'title' => 'Discovery',
+  'genre' => 'Electronic'
+  })
+
+album4.save()
+
+album5 = Album.new({
+  'artist_id' => artist3.id,
+  'title' => 'The Man-Machine',
+  'genre' => 'Electronic'
+  })
+
+album5.save()
+
+album6 = Album.new({
+  'artist_id' => artist3.id,
+  'title' => 'Computer World',
+  'genre' => 'Electronic'
+  })
+
+album6.save()
+
 
 binding.pry
 nil
