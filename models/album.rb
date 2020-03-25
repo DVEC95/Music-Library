7f95aa469facf7c0c5e141f2810cff63ac35b927
David Eastcroft <davideastcroft95@gmail.com>
2020-03-25 16:28:51 +0000

    Adds method for listing all albums.

--- models/album.rb
+++ models/album.rb
@@ -28,4 +28,10 @@ class Album
     @id = SQLRunner.run(sql, values)[0]['id'].to_i
   end
 
+  def self.all
+    sql = "SELECT * FROM albums"
+    albums = SQLRunner.run(sql, [])
+    return albums.map {|album| Album.new(album)}
+  end
+
 end
