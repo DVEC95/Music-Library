990d4c18b32d3948096a5efd0d679ba642dab07f
David Eastcroft <davideastcroft95@gmail.com>
2020-03-25 18:30:56 +0000

    Adds methods for finding albums by artist name and artist name by album title.

--- models/album.rb
+++ models/album.rb
@@ -1,4 +1,5 @@
-require_relative('../db/sql_runner.rb')
+require_relative('../db/sql_runner')
+require_relative('./artist')
 
 class Album
 
@@ -13,7 +14,7 @@ class Album
   end
 
   def save()
-    sql = "INSERT INTO albums
+    sql = 'INSERT INTO albums
     (
       artist_id,
       title,
@@ -23,20 +24,25 @@ class Album
     (
       $1, $2, $3
     )
-    RETURNING id"
+    RETURNING id'
     values = [@artist_id, @title, @genre]
     @id = SQLRunner.run(sql, values)[0]['id'].to_i
   end
 
   def self.all
-    sql = "SELECT * FROM albums"
+    sql = 'SELECT * FROM albums'
     albums = SQLRunner.run(sql, [])
     return albums.map {|album| Album.new(album)}
   end
 
-  def self.find(id)
+  def self.find_albums(artist)
+    sql = 'SELECT * FROM artists WHERE name = $1'
+    values = [artist]
+    artists = SQLRunner.run(sql, values)
+
+    artist_id = artists[0]['id'].to_i
     sql = 'SELECT * FROM albums WHERE artist_id = $1'
-    values = [id]
+    values = [artist_id]
     albums = SQLRunner.run(sql, values)
     return albums.map {|album| Album.new(album)}
   end
@@ -45,8 +51,13 @@ class Album
     sql = 'SELECT artist_id FROM albums WHERE title = $1'
     values = [album_title]
     results = SQLRunner.run(sql, values)
-    artist_id = results[0]
-    return artist_id
+    artist_id = results[0]['artist_id'].to_i
+
+    sql = 'SELECT * FROM artists WHERE id = $1'
+    values = [artist_id]
+    results = SQLRunner.run(sql, values)
+    artist = results[0]['name']
+    return artist
   end
 
 end
